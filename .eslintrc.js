e00720662b91d5efc1a8095ac346e2877ea87ed6
muhammadsubhankhan <ahsan.qureshi@bazaarvoice.com>
2024-03-30 16:03:58 +0500

    added eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,24 @@ module.exports = {
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
+        'import/prefer-default-export': 'off',
+        'max-classes-per-file': 'off',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/no-unused-vars': [
+            'error',
+            { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+        ],
+        '@typescript-eslint/no-use-before-define': 'off',
+        'no-console': ['error', { allow: ['warn', 'error'] }],
+        'sort-imports': [
+            'error',
+            {
+                ignoreCase: false,
+                ignoreDeclarationSort: false,
+                ignoreMemberSort: false,
+                memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+                allowSeparatedGroups: false,
+            },
+        ],
     },
 }
